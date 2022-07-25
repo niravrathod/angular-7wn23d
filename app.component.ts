5b931b23abb3b6fd366b2443adfc7b7d3bc1aaca
niravrathod <niravrathod473@gmail.com>
2022-07-25 17:44:06 +0530

    1 file modified

--- app.component.ts
+++ app.component.ts
@@ -227,12 +227,12 @@ export class AppComponent implements OnInit {
         document
           .querySelectorAll('li#collapseall')[0]
           .setAttribute('style', 'display: block;');
-        if(arg.column.freeze==undefined && this.can_be_deleted==true)
-        {
+        // if(arg.column.freeze==undefined && this.can_be_deleted==true)
+        // {
           document
           .querySelectorAll('li#freezeleft')[0]
           .setAttribute('style', 'display: block;');
-        }
+        // }
       } else {
         document
           .querySelectorAll('li#expandall')[0]
